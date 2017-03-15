914b1d709b3389e944ec5a325f2168f1e2c9b718
ToqaEid <toqa.eid@gmail.com>
2017-03-15 16:21:16 +0200

    TripDetails updates

--- Mashaweer/app/src/main/java/com/jets/mashaweer/TripDetails.java
+++ Mashaweer/app/src/main/java/com/jets/mashaweer/TripDetails.java
@@ -6,7 +6,11 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 public class TripDetails extends AppCompatActivity {
 
@@ -25,5 +29,37 @@ public class TripDetails extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        FloatingActionButton fab_play = (FloatingActionButton) findViewById(R.id.play_floating_button);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
+    /*====================== MENU ==============================*/
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_trip_details, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                deleteTrip();
+                return true;
+            
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void deleteTrip() {
+        Toast.makeText(this, "delete trip", Toast.LENGTH_SHORT).show();
+    }
+    /*==== END MENU ===*/
 }
